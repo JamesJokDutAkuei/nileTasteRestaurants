ec112dc100b6d8a2ff2be9de0d73be58b9009421
JamesJokDutAkuei <j.akuei@alutudent.com>
2025-04-01 21:22:10 +0200

    nileTaste main files

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// Initialize Supabase
+const supabaseUrl = 'https://spblrvdnzybwqwltkwqd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwYmxydmRuenlid3F3bHRrd3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2NTgxMDMsImV4cCI6MjA1ODIzNDEwM30.6ZeaLokILkck9xaxMNzZgt50ngISxKXrlrKF5AaAv6U';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+    
